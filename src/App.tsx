d2c88de18da00ae84f11abf0b4b26235ce9573bc
SayliDev <artaxw1@gmail.com>
2025-01-23 21:27:53 +0100

    feat: add protected route for authenticated users

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { AnimatePresence } from "framer-motion";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ProtectedRoute } from "./components/auth/ProtectedRoute";
 import { ThemeProvider } from "./components/theme-provider";
 import ChatPage from "./pages/ChatPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import { AnimatePresence } from "framer-motion";
 
 function App() {
   return (
@@ -11,7 +12,14 @@ function App() {
       <BrowserRouter>
         <AnimatePresence mode="wait">
           <Routes location={location} key={location.pathname}>
-            <Route path="/" element={<ChatPage />} />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <ChatPage />
+                </ProtectedRoute>
+              }
+            />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
           </Routes>
