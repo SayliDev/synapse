416864b60ccf10717da6f4d132504829dec2bf9c
SayliDev <artaxw1@gmail.com>
2025-01-24 19:19:27 +0100

    fix(auth): handle loading state in ProtectedRoute

--- src/components/auth/ProtectedRoute.tsx
+++ src/components/auth/ProtectedRoute.tsx
@@ -1,10 +1,18 @@
+import LoadingSpinner from "@/components/LoadingSpinner";
 import { useAuth } from "@/hooks/useAuth";
 import { Navigate } from "react-router-dom";
 
 export const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
 
   if (!user) {
+    console.log("User not authenticated");
+    console.log(user);
+
     return <Navigate to="/login" />;
   }
 
